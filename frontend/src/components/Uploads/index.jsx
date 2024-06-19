cab036f345cf0abbee10c35d8d270975aeedea2d
patilmanas04 <patilmanas1304@gmail.com>
2024-06-20 03:55:42 +0530

    Add images upload functionality

--- frontend/src/components/Uploads/index.jsx
+++ frontend/src/components/Uploads/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 const UploadsWrapper = styled.div`
@@ -53,18 +53,55 @@ const UploadButton = styled.button`
 `
 
 const Uploads = () => {
+	const [image, setImage] = useState(null)
+	const [video, setVideo] = useState(null)
+
+	const uploadImage = async (type) => {
+		const cloudinaryCloudName = "dgszxxbcl"
+		const url = `https://api.cloudinary.com/v1_1/${cloudinaryCloudName}/${type}/upload`
+		const formData = new FormData()
+		formData.append('file', image)
+		formData.append('upload_preset', 'images_preset')
+
+		try{
+			const response = await fetch(url, {
+				method: 'POST',
+				body: formData
+			})
+
+			const recievedImageUrl = await response.json()
+			return recievedImageUrl.url
+		}
+		catch(error){
+			console.error(error)
+		}
+
+	}
+
+	const handleImageSubmit = async (e) => {
+		try{
+			e.preventDefault()
+			const imageUrl = await uploadImage('image')
+			console.log(imageUrl)
+			console.log('Image Uploaded Successfully!!')
+		}
+		catch(error){
+			console.error(error)
+		}
+	}
+
 	return (
 		<UploadsWrapper>
 			<UploadsTitle>Upload Images or Videos</UploadsTitle>
 			<FormsContainer>
-				<UploadForm>
+				<UploadForm onSubmit={handleImageSubmit}>
 					<UploadLabel htmlFor="imageUpload">Upload Image</UploadLabel>
-					<UploadInput type="file" id="imageUpload" name="imageUpload" accept="image/*" />
+					<UploadInput type="file" id="imageUpload" name="imageUpload" accept="image/*" onChange={(e) => setImage((prev) => e.target.files[0])}/>
 					<UploadButton type='submit'>Upload Image</UploadButton>
 				</UploadForm>
 				<UploadForm>
 					<UploadLabel htmlFor="videoUpload">Upload Video</UploadLabel>
-					<UploadInput type="file" id="videoUpload" name="videoUpload" accept="video/*" />
+					<UploadInput type="file" id="videoUpload" name="videoUpload" accept="video/*" onChange={(e) => setVideo((prev) => e.target.files[0])}/>
 					<UploadButton type='submit'>Upload Video</UploadButton>
 				</UploadForm>
 			</FormsContainer>
