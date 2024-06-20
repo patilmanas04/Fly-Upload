1512e4c171f094524873b2e6e55f3a0f6481572f
patilmanas04 <patilmanas1304@gmail.com>
2024-06-21 02:58:56 +0530

    Modify the files upload functionality and use cloudinary sdk for uploads

--- frontend/src/contexts/MediaState.jsx
+++ frontend/src/contexts/MediaState.jsx
@@ -6,46 +6,20 @@ const MediaState = ({ children }) => {
     const authToken = localStorage.getItem("authToken");
     const [gallery, setGallery] = useState([]);
 
-    const getMediaDetails = (media) => {
-		const mediaDetails = {
-			name: media.name,
-			size: media.size/1000000
-		}
-		
-		console.log(mediaDetails)
-
-		return mediaDetails
-	}
-
     const uploadMedia = async (media, type) => {
-        const cloudinaryCloudName = "dgszxxbcl"
-        const url = `https://api.cloudinary.com/v1_1/${cloudinaryCloudName}/${type}/upload`
-        const formData = new FormData()
-        formData.append('file', media)
-        formData.append('upload_preset', type === "image" ? 'images_preset' : 'videos_preset')
-
-        const { name, size } = getMediaDetails(media)
-
-        try{
-			const response = await fetch(url, {
-				method: 'POST',
-				body: formData
-			})
-
-			const recievedImageUrl = await response.json()
+		try{
+			const formData = new FormData()
+			formData.append('file', media)
+			formData.append('title', media.name)
+			formData.append('mediaType', type)
+			formData.append('mediaSize', `${(media.size/100000).toFixed(2)} MB`)
 
 			const serverResponse = await fetch(`${host}/api/media/upload`, {
 				method: 'POST',
 				headers: {
-					'Content-Type': 'application/json',
 					'Auth-Token': localStorage.getItem('authToken'),
 				},
-				body: JSON.stringify({
-					title: name,
-					mediaType: type,
-					mediaLink: recievedImageUrl.url,
-					mediaSize: `${size} MB`
-				})
+				body: formData
 			})
 
 			const data = await serverResponse.json()
